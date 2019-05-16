9101cafae4f7f06d45c9e31fafa0bb6d3b0147d2
Asepabdaz <asepabdaz@gmail.com>
2019-05-16 10:18:14 +0700

    Initial Commit

---
+++ UIText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIText
+//
+//  Created by asep abdaz on 16/05/19.
+//  Copyright © 2019 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
